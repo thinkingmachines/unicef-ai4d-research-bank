a89380e59fd31ad6b10539227905ff888400376a
Bianca Silmaro <bianca@thinkingmachin.es>
2023-03-14 14:21:11 +0800

    Integrate fetch with frontend

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,8 +2,15 @@ import type { CatalogueItemType } from 'types/CatalogueItem.type'
 
 // eslint-disable-next-line import/prefer-default-export
 export const fetchCatalogItems = async (): Promise<CatalogueItemType[]> => {
-	const response = await fetch('api/data/catalog.json') // Replace with your own JSON file path
+	const response = await fetch('/api/data/catalog.json') // Replace with your own JSON file path
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 	const catalog: CatalogueItemType[] = await response.json()
 	return catalog
+}
+
+export const fetchCatalogItem = async (
+	catalogItemId?: string
+): Promise<CatalogueItemType | undefined> => {
+	const catalogItems: CatalogueItemType[] = await fetchCatalogItems()
+	return catalogItems.find(item => item.id === catalogItemId)
 }
--- src/pages/CataloguePage.tsx
+++ src/pages/CataloguePage.tsx
@@ -1,7 +1,8 @@
 import { AutoComplete, DatePicker, Select, Space } from 'antd'
+import { fetchCatalogItems } from 'api'
 import CatalogueItemCard from 'components/CatalogueItemCard'
+import { useEffect, useState } from 'react'
 import type { CatalogueItemType } from 'types/CatalogueItem.type'
-import CatalogueItemsMock from '../mocks/CatalogueItems.data.json'
 
 const { RangePicker } = DatePicker
 
@@ -10,7 +11,43 @@ const onTagsChange = () => {}
 const onOrganizationChange = () => {}
 
 const CataloguePage = () => {
-	const catalogueItems = CatalogueItemsMock.items as CatalogueItemType[]
+	const [isLoading, setIsLoading] = useState(false)
+	const [catalogueItems, setCatalogueItems] = useState<CatalogueItemType[]>([])
+	let catalogueItemsSection
+
+	const fetchData = async () => {
+		try {
+			setIsLoading(true)
+			const nextCatalogueItems = await fetchCatalogItems()
+			setCatalogueItems(nextCatalogueItems)
+		} catch (error) {
+			// eslint-disable-next-line no-console
+			console.error(error)
+		} finally {
+			setIsLoading(false)
+		}
+	}
+
+	useEffect(() => {
+		void fetchData()
+	}, [])
+
+	if (isLoading) {
+		catalogueItemsSection = <span>Loading...</span>
+	}
+
+	if (!isLoading && catalogueItems.length === 0) {
+		catalogueItemsSection = <span>No catalogue items available.</span>
+	}
+
+	if (!isLoading && catalogueItems.length > 0) {
+		catalogueItemsSection = catalogueItems.map(catalogueItem => (
+			<CatalogueItemCard
+				key={catalogueItem.id}
+				catalogueItemData={catalogueItem}
+			/>
+		))
+	}
 
 	return (
 		<div className='h-[calc(100vh_-_3rem)] bg-white'>
@@ -81,12 +118,7 @@ const CataloguePage = () => {
 							option!.value.toUpperCase().includes(inputValue.toUpperCase())
 						}
 					/>
-					{catalogueItems.map(catalogueItem => (
-						<CatalogueItemCard
-							key={catalogueItem.id}
-							catalogueItemData={catalogueItem}
-						/>
-					))}
+					<div className='mt-3 text-cloud-burst'>{catalogueItemsSection}</div>
 				</div>
 			</div>
 		</div>
