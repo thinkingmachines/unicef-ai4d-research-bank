a82f9ad4c6e3b81dfb13f269ea65a5e08cf1206d
Butch Landingin <butch@thinkingmachin.es>
2023-03-15 16:45:23 +0800

    feat:Add getCountryList to api

--- src/api/index.ts
+++ src/api/index.ts
@@ -14,3 +14,11 @@ export const fetchCatalogItem = async (
 	const catalog = await fetchCatalogItems()
 	return catalog.find(item => item.id === id)
 }
+
+export const getCountryList = async (): Promise<(string | undefined)[]> => {
+	const catalog = await fetchCatalogItems()
+	const countries = catalog
+		.filter(item => 'country-region' in item)
+		.map(item => item['country-region'])
+	return [...new Set(countries)]
+}
