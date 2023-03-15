bf37e368f04c3c1558fde3545c8ce88529687256
Butch Landingin <butch@thinkingmachin.es>
2023-03-15 12:31:59 +0800

    feat: Replace mock with fetchCatalogItem from api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable unicorn/no-null */
 import type { CatalogueItemType } from 'types/CatalogueItem.type'
 import HOME_PATH from '../constants'
 
@@ -9,8 +10,13 @@ export const fetchCatalogItems = async (): Promise<CatalogueItemType[]> => {
 }
 
 export const fetchCatalogItem = async (
-	catalogItemId?: string
-): Promise<CatalogueItemType | undefined> => {
-	const catalogItems: CatalogueItemType[] = await fetchCatalogItems()
-	return catalogItems.find(item => item.id === catalogItemId)
+	id: string
+): Promise<CatalogueItemType | null> => {
+	const catalog = await fetchCatalogItems()
+	const matchCatalog = catalog.filter(item => item.id === id)
+	if (matchCatalog.length > 0) {
+		const nextItem = matchCatalog[0]
+		return nextItem
+	}
+	return null
 }
