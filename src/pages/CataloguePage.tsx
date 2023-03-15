884151851ff1f29bf7a0567f2d3c20fe6984227b
Bianca Silmaro <bianca@thinkingmachin.es>
2023-03-15 10:28:07 +0800

    Add filter skeletons in the catalogue

--- src/pages/CataloguePage.tsx
+++ src/pages/CataloguePage.tsx
@@ -1,6 +1,13 @@
+import { AutoComplete, DatePicker, Select, Space } from 'antd'
 import CatalogueItemCard from 'components/CatalogueItemCard'
 import type { CatalogueItemType } from 'types/CatalogueItem.type'
 import CatalogueItemsMock from '../mocks/CatalogueItems.data.json'
+
+const { RangePicker } = DatePicker
+
+const onCountryRegionChange = () => {}
+const onTagsChange = () => {}
+const onOrganizationChange = () => {}
 
 const CataloguePage = () => {
 	const catalogueItems = CatalogueItemsMock.items as CatalogueItemType[]
@@ -11,11 +18,68 @@ const CataloguePage = () => {
 				Search Catalogue
 			</div>
 			<div className='my-8 flex flex-row px-10'>
-				<div className='w-1/3 font-bold text-cloud-burst'>Filters</div>
+				<div className='w-1/3 px-10'>
+					<span className='font-bold text-cloud-burst'>Filters</span>
+					<Space style={{ width: '100%' }} direction='vertical'>
+						<div className='pt-3'>
+							<span className='font-bold text-cloud-burst'>Country/Region</span>
+							<Select
+								mode='multiple'
+								allowClear
+								style={{ width: '100%', marginTop: '8px' }}
+								placeholder='Select a country/region...'
+								defaultValue={[]}
+								onChange={onCountryRegionChange}
+								options={[{ label: 'Timor Leste', value: 'Timor Leste' }]}
+							/>
+						</div>
+
+						<div className='pt-3'>
+							<span className='font-bold text-cloud-burst'>Date Created</span>
+							<RangePicker style={{ width: '100%', marginTop: '8px' }} />
+						</div>
+
+						<div className='pt-3'>
+							<span className='font-bold text-cloud-burst'>Date Updated</span>
+							<RangePicker style={{ width: '100%', marginTop: '8px' }} />
+						</div>
+
+						<div className='pt-3'>
+							<span className='font-bold text-cloud-burst'>Organization</span>
+							<Select
+								mode='multiple'
+								allowClear
+								style={{ width: '100%', marginTop: '8px' }}
+								placeholder='Select an organization...'
+								defaultValue={[]}
+								onChange={onOrganizationChange}
+								options={[{ label: 'Timor Leste', value: 'Timor Leste' }]}
+							/>
+						</div>
+
+						<div className='pt-3'>
+							<span className='font-bold text-cloud-burst'>Tags</span>
+							<Select
+								mode='tags'
+								allowClear
+								style={{ width: '100%' }}
+								placeholder='Select a tag...'
+								onChange={onTagsChange}
+								options={[
+									{ label: 'poverty-mapping', value: 'poverty-mapping' }
+								]}
+							/>
+						</div>
+					</Space>
+				</div>
 				<div className='flex w-2/3 flex-col'>
-					<input
+					<AutoComplete
+						style={{ width: '100%' }}
+						options={[{ value: 'Poverty Mapping for Timor Leste' }]}
 						placeholder='Search for a dataset or a model'
-						className='appearance-none rounded border py-2 px-3 text-cloud-burst shadow'
+						filterOption={(inputValue, option) =>
+							option!.value.toUpperCase().includes(inputValue.toUpperCase())
+						}
 					/>
 					{catalogueItems.map(catalogueItem => (
 						<CatalogueItemCard
