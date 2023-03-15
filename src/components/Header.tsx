2161d391663d97800f4bbb6311785630ad6d9a04
Bianca Silmaro <bianca@thinkingmachin.es>
2023-03-15 23:37:51 +0800

    Add styling to landing page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,18 @@ import HOME_PATH from '../constants'
 
 const Header = () => (
 	<div className='flex h-12 justify-between bg-white p-3 px-10 text-cloud-burst'>
-		<Link to={`${HOME_PATH}/`}>AI4D Research Bank</Link>
-		<Link to={`${HOME_PATH}/catalogue`}>Catalogue</Link>
+		<Link
+			to={`${HOME_PATH}/`}
+			className='font-semibold hover:text-gray-600 hover:duration-500'
+		>
+			AI4D Research Bank
+		</Link>
+		<Link
+			to={`${HOME_PATH}/catalogue`}
+			className='font-medium text-gray-500 hover:text-cloud-burst hover:duration-500'
+		>
+			Catalogue
+		</Link>
 	</div>
 )
 
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,33 +1,47 @@
+import LandingHeroImg from '../assets/landing-hero-bg.jpg'
+import Tag from '../components/Tag'
+
 const LandingPage = () => (
 	<div className='h-[calc(100vh_-_3rem)] bg-white'>
-		<div className='flex h-96 flex-col items-center justify-center bg-cloud-burst px-10 text-white'>
-			<span className='mb-3 text-center text-3xl font-bold text-white'>
-				Accelerating machine learning adoption in the development sector
-			</span>
-			<span className='px-10 text-center'>
-				Browse our catalogue of models and datasets to gain access to code,
-				documentation, and pre-processed datasets that fit to your needs
-			</span>
-			<input
-				className='mt-8 w-3/5 rounded-lg p-3 text-cloud-burst'
-				placeholder='Search for a dataset, model, or tag'
+		<div className='relative flex h-96 flex-col items-center justify-center px-10 text-white'>
+			<img
+				src={LandingHeroImg}
+				alt='BannerImage'
+				className='absolute z-0 h-full w-full object-cover'
 			/>
+			<div className='absolute z-10 h-full w-full bg-cloud-burst opacity-60' />
+			<div className='z-20 flex flex-col items-center justify-center'>
+				<span className='mb-3 text-center text-4xl font-medium tracking-tighter text-white'>
+					Accelerating machine learning adoption in the development sector
+				</span>
+				<span className='px-10 text-center'>
+					Browse our catalogue of models and datasets to gain access to code,
+					documentation, and pre-processed datasets that fit to your needs
+				</span>
+				<input
+					className='mt-5 w-3/5 rounded-md px-3 py-2 tracking-tight text-cloud-burst'
+					placeholder='Search for a dataset or a model'
+				/>
+			</div>
 		</div>
-		<div className='p-10'>
-			<span className='font-bold text-cloud-burst'>Browse by tags</span>
-			<div>
-				<button
-					type='button'
-					className='btn btn-cloud-burst rounded bg-cloud-burst p-1 text-white'
-				>
-					poverty-mapping
-				</button>
-				<button
-					type='button'
-					className='btn btn-cloud-burst m-2 rounded bg-cloud-burst p-1 text-white'
-				>
-					air-quality
-				</button>
+		<div className='flex flex-row'>
+			<div className='flex w-1/2 flex-col p-10'>
+				<span className='font-semibold tracking-normal text-cloud-burst'>
+					BROWSE BY TAGS
+				</span>
+				<div className='flex gap-3 py-3'>
+					<Tag>poverty-mapping</Tag>
+					<Tag>air-quality</Tag>
+				</div>
+			</div>
+			<div className='flex w-1/2 flex-col p-10'>
+				<span className='font-semibold tracking-normal text-cloud-burst'>
+					BROWSE BY COUNTRY/REGION
+				</span>
+				<div className='flex gap-3 py-3'>
+					<Tag>philippines</Tag>
+					<Tag>timor-leste</Tag>
+				</div>
 			</div>
 		</div>
 	</div>
