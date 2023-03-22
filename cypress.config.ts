bb97f27b42f5ee1bc8de95672676769f63a4f470
Butch Landingin <butch@thinkingmachin.es>
2023-03-22 14:58:43 +0800

    feat: Add initial nav tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
 	fileServerFolder: 'dist',
 	fixturesFolder: false,
-	projectId: 'etow1b',
 	e2e: {
-		baseUrl: 'http://localhost:4173/',
+		chromeWebSecurity: false,
+		// baseUrl: 'https://thinkingmachines.github.io/unicef-ai4d-research-bank/',
+		baseUrl: 'http://localhost:4173/unicef-ai4d-research-bank/',
 		specPattern: 'cypress/e2e/**/*.ts'
 	}
 })
