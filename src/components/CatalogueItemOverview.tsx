885ebd5280007b77a8d5780fb1532bd2a5f94875
Bianca Silmaro <bianca@thinkingmachin.es>
2023-03-29 23:26:25 +0800

    Redesign evaluation metric

--- src/components/CatalogueItemOverview.tsx
+++ src/components/CatalogueItemOverview.tsx
@@ -1,3 +1,4 @@
+import { InfoCircleOutlined } from '@ant-design/icons'
 import type { CatalogueItemType } from 'types/CatalogueItem.type'
 
 interface Props {
@@ -11,34 +12,52 @@ const CatalogueItemOverview = ({ catalogueItem }: Props) => {
 		const { metric, link } = evalItem
 		// Metric and link can be undefined so there is a need to check if it exists
 		const metricItem = metric && (
-			<>
-				{metric['metric-type']} ({metric.value})
-			</>
+			<div className='flex flex-col'>
+				<span className='text-xs font-semibold text-cloud-burst'>
+					{metric['metric-type'].toUpperCase()}
+				</span>
+				<span className='text-2xl font-semibold text-cloud-burst'>
+					{metric.value}
+				</span>
+			</div>
 		)
 		const linkItem = link && (
-			<a
-				href={link.url}
-				target='_blank'
-				rel='noreferrer'
-				className='hover:underline'
-			>
-				{link.description}
-			</a>
+			<div className='self-center'>
+				<InfoCircleOutlined style={{ marginRight: '8px' }} />
+				<a
+					href={link.url}
+					target='_blank'
+					rel='noreferrer'
+					className='hover:underline'
+				>
+					{link.description}
+				</a>
+			</div>
 		)
 		if (metric && link) {
 			return (
-				<div className='' key={metric['metric-type']}>
+				<div
+					className='align-center mt-3 flex grid-rows-1 flex-row gap-5 p-5'
+					key={metric['metric-type']}
+				>
 					{metricItem}
-					<br />
 					{linkItem}
 				</div>
 			)
 		}
 		if (metric) {
-			return <div key={metric['metric-type']}>{metricItem}</div>
+			return (
+				<div className='p-5' key={metric['metric-type']}>
+					{metricItem}
+				</div>
+			)
 		}
 		if (link) {
-			return <div key={link.url}>{linkItem}</div>
+			return (
+				<div className='p-5' key={link.url}>
+					{linkItem}
+				</div>
+			)
 		}
 		return '-'
 	})
@@ -47,8 +66,8 @@ const CatalogueItemOverview = ({ catalogueItem }: Props) => {
 			<p className='mb-5 text-gray-600'>{description}</p>
 			{evaluationMetric !== undefined && (
 				<div className='flex flex-col'>
-					<span className='font-semibold'>Evaluation Metric</span>
-					<div className='flex flex-col gap-3 text-gray-600'>
+					<span className='font-semibold text-gray-600'>Evaluation Metric</span>
+					<div className='grid-cols-1 gap-5 divide-y divide-gray-200 text-gray-600'>
 						{evaluationMetricSection}
 					</div>
 				</div>
