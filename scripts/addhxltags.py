729cc37d031baf004e1e489409712c8620590973
Butch Landingin <butch@thinkingmachin.es>
2023-04-03 14:43:43 +0800

    feat:Convert hardcoded params into command line options for adding hxl tags

--- scripts/addhxltags.py
+++ scripts/addhxltags.py
@@ -1,56 +1,99 @@
+import argparse
 import json
 from pathlib import Path
 
 import hxl
 import hxl.converters
+import hxl.input
+from validate_catalog import validate_url
+
+from utils import (
+    CSVResponseInput,
+    get_gdown_response,
+    is_gdrive_url,
+    is_github_url,
+    transform_gdrive_url,
+    transform_github_url,
+)
+
+parser = argparse.ArgumentParser(description="Add hxl tags to a file")
+parser.add_argument(
+    "-i", "--input", metavar="filename1/url", type=str, help="input file path or url"
+)
+parser.add_argument(
+    "-o", "--output", metavar="filename2", type=str, help="output file path"
+)
+parser.add_argument(
+    "-t", "--tags", metavar="filename3", type=str, help="json tags file path"
+)
 
 
 def main():
-    fname = "tests/testdata/test-un-patched.csv"
-    unpatched = Path(fname)
-    if not unpatched.exists():
-        print(f"Invalid file {fname} does not exist")
+    args = parser.parse_args()
+
+    if not args.input:
+        print("Missing input file or url, specify -i or --input")
+        return 1
+    if not args.output:
+        print("Missing output file, specify -o or --output")
+        return 1
+    if not args.tags:
+        print("Missing tags file, specify -t or --tags")
+        return 1
+    if args.input.startswith("http://") or args.input.startswith("https://"):
+        unpatched = args.input
+        if is_github_url(unpatched):
+            unpatched = transform_github_url(unpatched)
+        elif is_gdrive_url(unpatched):
+            unpatched = transform_gdrive_url(unpatched)
+        if not validate_url(unpatched, args.input):
+            return 1
+        input_options = None
+    else:
+        # assume file
+        unpatched = Path(args.input)
+        if not unpatched.exists():
+            print(f"Invalid input file: {unpatched} not found")
+            return 1
+        unpatched = unpatched.as_posix()
+        input_options = hxl.input.InputOptions(allow_local=True)
+    patched = Path(args.output)
+    if not patched.parent.is_dir():
+        print(
+            f"output directory {patched.parent} for output file {patched.name} does not exist. Create it first."
+        )
         return 1
 
-    tagging_specs = [
-        ("date", "#date+reported"),
-        ("station_code", "#loc+code"),
-        ("station", "#loc+name"),
-        ("city", "#adm3+name"),
-        ("longitude", "#geo+lon"),
-        ("latitude", "#geo+lat"),
-        ("geometry", "#geo+coord"),
-        ("bbox", "#geo+bounds"),
-        ("total_population", "#population"),
-        ("ADM0_EN", "#country+name"),
-        ("ADM0_PCODE", "#country+code+v_pcodes"),
-        ("ADM0_TH", "#country+name+i_th"),
-        ("ADM1_EN", "#adm1+name"),
-        ("ADM1_PCODE", "#adm1+code+v_pcodes"),
-        ("ADM1_TH", "#adm1+name+i_th"),
-        ("ADM2_EN", "#adm2+name"),
-        ("ADM2_PCODE", "#adm2+code+v_pcodes"),
-        ("ADM2_TH", "#adm2+name+i_th"),
-        ("ADM3_EN", "#adm3+name"),
-        ("ADM3_PCODE", "#adm3+code+v_pcodes"),
-        ("ADM3_TH", "#adm3+name+i_th"),
-        ("validOn", "#date+start"),
-        ("validTo", "#date+end"),
-    ]
-    filename = unpatched.as_posix()
+    tagfile = Path(args.tags)
+    if not tagfile.exists():
+        print(f"Invalid tags file: {tagfile} not found")
+        return 1
+    tagging_specs = []
+    with open(tagfile) as f:
+        tagging_specs = json.load(f)
+    if is_gdrive_url(unpatched):
+        resp, sess = get_gdown_response(unpatched)
+        if resp is None:
+            print(
+                f"Invalid gdrive url {unpatched}: Could not access url. Possibly not publically shared."
+            )
+        csv_input = CSVResponseInput(resp, sess)
+    else:
+        csv_input = hxl.input.make_input(unpatched, input_options=input_options)
     source = hxl.data(
         hxl.converters.Tagger(
-            hxl.input.make_input(filename, hxl.input.InputOptions(allow_local=True)),
+            csv_input,
             tagging_specs,
         )
     ).cache()
-    with open("tests/testdata/testoutput/test-patched.csv", "w", encoding="utf-8") as f:
+
+    with open(
+        patched,
+        "w",
+        encoding="utf-8",
+    ) as f:
         # Write out a dataset as CSV
         hxl.input.write_hxl(f, source)
-    with open(
-        "tests/testdata/testoutput/test-un-patched-tagspecs.json", "w", encoding="utf-8"
-    ) as f:
-        json.dump(tagging_specs, f)
     return 0
 
 
