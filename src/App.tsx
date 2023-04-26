78e3b7cd83b31d7260983ed74f16ace871f0fdd9
Bianca Silmaro <bianca@thinkingmachin.es>
2023-04-26 22:49:17 +0800

    Remove unused load catalog page

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { CatalogueItemProvider } from 'context/CatalogueItemContext'
 import { FilterProvider } from 'context/FilterContext'
 import { SearchProvider } from 'context/SearchContext'
 import { CatalogueItemPage, CataloguePage, LandingPage } from 'pages'
-import LoadCatalog from 'pages/LoadCatalog'
 import type { ReactElement } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HOME_PATH from './constants'
@@ -29,10 +28,6 @@ export default function App(): ReactElement {
 						<SearchProvider>
 							<Routes>
 								<Route path={`${HOME_PATH}/`} element={<LandingPage />} />
-								<Route
-									path={`${HOME_PATH}/loadcatalog`}
-									element={<LoadCatalog />}
-								/>
 								<Route path={`${HOME_PATH}/catalogue`}>
 									<Route index element={<CataloguePage />} />
 									<Route path=':id' element={<CatalogueItemPage />} />
