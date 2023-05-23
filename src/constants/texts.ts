9711dcafee5d22acd2b5c8ad8651e22faab16dce
GERLITO CHAGAS <ger@thinkingmachin.es>
2023-05-23 11:11:36 +0800

    add catalogue item map preview

---
+++ src/constants/texts.ts
@@ -0,0 +1,4 @@
+const MAP_PREVIEW_NOTE =
+	'Note: The following visualization represents the geographical scope of the dataset. [Disclaimer on the map visualization, this language can be improved.]'
+
+export default MAP_PREVIEW_NOTE
