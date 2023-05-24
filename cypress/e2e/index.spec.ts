ea3078ea55c10e0f83b02b42fa8de889e8e54c76
GERLITO CHAGAS <ger@thinkingmachin.es>
2023-05-24 11:23:32 +0800

    make index.spec work with pagination

--- cypress/e2e/index.spec.ts
+++ cypress/e2e/index.spec.ts
@@ -2,22 +2,29 @@ describe('Access all resources', () => {
 	it('renders the main page ', () => {
 		cy.visit('/')
 	})
+
 	it('fetch the catalog json file ', () => {
 		cy.request('/api/data/catalog.json')
 	})
+
 	it('should navigate to the catalog page on click', () => {
 		cy.contains('Catalogue').click()
 		cy.location('pathname').should('eq', `/unicef-ai4d-research-bank/catalogue`)
 	})
+
 	it('should navigate to the catalog item page on click', () => {
 		// eslint-disable-next-line cypress/no-unnecessary-waiting
 		cy.wait(3000)
-		cy.contains('Poverty Mapping Model for Timor Leste').click()
+		cy.contains(
+			'Poverty Mapping Rollout Dataset for Timor Leste (2016)'
+		).click()
+
 		cy.location('pathname').should(
 			'eq',
-			`/unicef-ai4d-research-bank/catalogue/povmap-timor-leste`
+			`/unicef-ai4d-research-bank/catalogue/povmap-timor-leste-rollout-dataset`
 		)
 	})
+
 	it('should navigate to the main page', () => {
 		cy.contains('AI4D Research Bank').click()
 		cy.location('pathname').should('eq', `/unicef-ai4d-research-bank/`)
