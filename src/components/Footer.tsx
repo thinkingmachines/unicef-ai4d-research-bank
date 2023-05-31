ac0f43bc782490398f43e0bd0090fbb362df225c
GERLITO CHAGAS <ger@thinkingmachin.es>
2023-05-31 09:10:14 +0800

    test: add contribution page tests

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,10 @@ import { Link } from 'react-router-dom'
 import HOME_PATH from '../constants'
 
 const Footer = () => (
-	<div className='flex h-12 justify-center bg-[#F9FAFB] p-3 px-10 text-[#82838D]'>
+	<div
+		className='flex h-12 justify-center bg-[#F9FAFB] p-3 px-10 text-[#82838D]'
+		data-cy='footer'
+	>
 		<div>
 			Want to contribute to the research bank?{' '}
 			<Link
